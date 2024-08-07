b8dae1fd67a8ad9f2076e0e58a1593622adf0589
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-08-07 23:48:00 +0530

    Update TodoITEM1.jsx

--- Projects/1-Todo-App/src/components /TodoITEM1.jsx
+++ Projects/1-Todo-App/src/components /TodoITEM1.jsx
@@ -1,20 +1,17 @@
 function TodoITEM1(){
     let todoName='Buy Milk';
     let todoDate="4/10/2023";
-    return <div class="container text-center">
+    return (<div class="container r">
     
 
-    <div class="row">
-      <div class="col-6">
-        { todoName}
-      </div>
-      <div class="col-4">
-       {todoDate}
-      </div>
+    <div class="row kg-row">
+      <div class="col-6"> { todoName}</div>
+      <div class="col-4">{todoDate}</div>
       <div class="col-2">
-      <button type="button" class="btn btn-danger">Delete</button>
+      <button type="button" class="btn btn-danger kg-button">Delete</button>
       </div>
     </div>
     </div>
+    );
 }
 export default TodoITEM1;
