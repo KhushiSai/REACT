ada96cd13b863d27b5171f86000adaf850578e0c
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-08-07 23:52:43 +0530

    Update App.jsx

--- Projects/1-Todo-App/src/App.jsx
+++ Projects/1-Todo-App/src/App.jsx
@@ -4,18 +4,21 @@ import TodoITEM1 from "./components /TodoITEM1";
 import TodoITEM2 from "./components /TodoITEM2";
 import "./App.css";
 function App(){
-  return
+  return(
 
-  <center class='todo-container'>
+  <center class="todo-container">
     <AppName/>
     <AddTodo/>
+    <div className="todo-items">
     <TodoITEM1></TodoITEM1>
     <TodoITEM2></TodoITEM2>
+      </div>
     
 
       
 
     
   </center>
+    );
 }
 export default App;
